9884a3094c3ee9c206174f76aeb51416339fddb0
pedroall <cttpedroall@gmail.com>
2024-10-26 02:24:33 +0000

    Tests for new features and for handling errors

---
+++ tests/run.spec.ts
@@ -0,0 +1,24 @@
+import { Context } from '../src/ruxy'
+import { test, expect } from '@jest/globals'
+
+const ENDL = '\n'
+
+test('It should call echo and return hello world', async () => {
+    const text = 'Foo, Bar.'
+    const ctx = new Context('echo', text).run()
+    const stdout = await ctx.stdout()
+
+    expect(stdout).toBe(text + ENDL)
+})
+
+test('It should return an error trying to run gcc with unknown directory', async () => {
+    const text = 'something random'
+
+    const ctx = new Context('gcc', text).run()
+
+    async function testStdout() {
+        return ctx.stdout()
+    }
+
+    expect(testStdout).rejects.toThrowError()
+})
